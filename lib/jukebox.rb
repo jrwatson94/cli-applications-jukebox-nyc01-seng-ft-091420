38fe9e205399b23cc56269bf35120c964b780d28
Reid Watson <j.watsonreid@gmail.com>
2020-09-14 03:05:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,19 +31,7 @@ def list (song_array)
   end
 end
 
-def play(songs)
-  puts "Please enter a song name or number:"
-  user_request = gets.chomp
-  songs.each_with_index do |song, i|
-    if user_request.to_i == i
-    puts "playing #{songs[i-1]}"
-  elsif user_request == song 
-  puts "playing #{song[i]}"
-else puts "Invalid input, please try again"
-  end 
-end 
 
-end  
 
 def exit_jukebox
   puts "Goodbye!"
@@ -68,7 +56,7 @@ def run(song_array)
     end
   end
 end
-#run(songs)
+run(songs)
 
 
 
