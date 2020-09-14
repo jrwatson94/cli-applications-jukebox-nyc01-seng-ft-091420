1c92d785a10afa0196c23283047b065b0cd2ad7d
Reid Watson <j.watsonreid@gmail.com>
2020-09-14 01:29:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,11 @@
 # Add your code here
 
 def help
-  
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+  puts "Please enter a command:"
+  input = gets.chomp
+end
